3983364f937df3befea75ae064f67b4fe85cfaaf
Vinícius <80495420+Virniciu@users.noreply.github.com>
2022-07-27 15:15:31 -0300

    Organizei melhor o código tamo junto

---
+++ casada.py
@@ -0,0 +1,10 @@
+casada = """9 Atitudes definitivas para conquistar uma mulher casada:
+1) Crie atração de maneira inconsciente
+2) Deixe seu interesse sexual implícito
+3) Faça-a se sentir única e especial
+4) Seja Cauteloso Com Suas Ações Publicas
+5) Seja um homem melhor que o marido
+6) Tome a atitude inicial
+7) Use o WhatsApp para criar conexão emocional
+8) Tenha Paciência Mesmo Depois da Conquista
+9) Deixe claro que ela pode confiar em você"""
---
+++ namorada.py
@@ -0,0 +1,13 @@
+namorada = """Como conseguir uma namorada em pouco tempo:
+1) Peça para algum amigo bancar o cupido
+2) Converse com algumas moças nas mídias sociais
+3) Baixe aplicativos de relacionamento
+4) Participe de novos grupos
+5) Aborde mulheres em público
+6) Adote uma linguagem corporal aberta
+7) Chame uma moça interessante para sair
+8) Tente conhecer a moça durante o encontro
+9) Passe mais tempo com a moça
+10) Descubra se vocês são compatíveis de verdade
+11) Peça a moça em namoro quando parecer a hora certa
+12) Recomece a sua busca se a moça não for a pessoa ideal"""
